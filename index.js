e1d43f7e755176937a4f2098515f6cdb572839bc
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-08-04 12:06:02 -0400

    commit tree f76ff907680d1f4cf914ffc623d319b19f98282a

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+let driver = {};
+
+function updateDriverWithKeyAndValue(obj, key, value) {
+  let keyValueObj = { [key]: value }
+  return Object.assign({}, obj, keyValueObj);
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(obj, key, value) {
+  obj[key] = value;
+  return obj;
+}
+
+function deleteFromDriverByKey(obj, key) {
+  const newObj = Object.assign({}, obj);
+  delete newObj[`${key}`];
+  return newObj;
+}
+
+function destructivelyDeleteFromDriverByKey(obj, key) {
+  delete obj[`${key}`];
+  return obj;
+}
